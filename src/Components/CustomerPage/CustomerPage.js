d5733499c104a38f7d0eb336bf3bb16a92111d7e
danRolon <daniel.rolon@yahoo.com>
2019-07-30 10:39:30 -0500

    commit to pull

--- src/Components/CustomerPage/CustomerPage.js
+++ src/Components/CustomerPage/CustomerPage.js
@@ -1,5 +1,5 @@
-import React,{useEffect,useState,useContext} from 'react';
-import {TokenContext} from '../Context/Contexts';
+import React, { useEffect, useState, useContext } from 'react';
+import { TokenContext } from '../Context/Contexts';
 
 
 import axios from "axios";
@@ -22,35 +22,31 @@ background-position: center center;
 background-repeat: none;
 `
 
-
-
-
 const CustomerPage = () => {
-  // const [starwarsChars, setChars] = useState()
-  const { token,setToken } = useContext(TokenContext);
+  const { token, setToken } = useContext(TokenContext);
   localStorage.getItem("token")
-    const [data, setData] = useState({});
-    
-localStorage.getItem('token')
-    useEffect(() => {
-      setToken(localStorage.getItem('token'))
-      console.log('test')
-    }, [token,setToken]);
-    const [secretData, setSecretData] = useState([]);
-    function setRestricted(res) {
-      setSecretData(res);
-    }
-    useEffect(() => {
-      AxiosWithAuth()
-        .get("http://localhost:4000/restricted/data")
-        .then(res => {
-          console.log(res);
-          setRestricted(res.data);
-        })
-        .catch(err => console.log(err));
-    }, []);
-    const fetchData = () => {
-      axios.get(`http://localhost:4000/api/restricted/data`)
+  const [data, setData] = useState({});
+
+  localStorage.getItem('token')
+  useEffect(() => {
+    setToken(localStorage.getItem('token'))
+    console.log('test')
+  }, [token, setToken]);
+  const [secretData, setSecretData] = useState([]);
+  function setRestricted(res) {
+    setSecretData(res);
+  }
+  useEffect(() => {
+    AxiosWithAuth()
+      .get("http://localhost:4000/restricted/data")
+      .then(res => {
+        console.log(res);
+        setRestricted(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+  const fetchData = () => {
+    axios.get(`http://localhost:4000/api/restricted/data`)
       .then(res => {
         console.log(res.data);
         setData(res.data);
@@ -58,65 +54,24 @@ localStorage.getItem('token')
       .catch(err => {
         console.log(err);
       })
-    }
-  console.log("token","does context get this")
-    useEffect(fetchData, [localStorage.getItem('token')]);
-  console.log(data,"data")
-//export const TokenContext = createContext();
-  return(
+  }
+  console.log("token", "does context get this")
+  useEffect(fetchData, [localStorage.getItem('token')]);
+  console.log(data, "data")
+  //export const TokenContext = createContext();
+  return (
     <div>
- <Sap className="App">
-   <main>
-     <WorkersGrid />
-   </main>
-<h1>Private Route</h1>
-{/* <WorkerType/> */}
-<CreateWorkerButton/>
+      <Sap className="App">
+        <main>
+          <WorkersGrid />
+        </main>
+        <h1>Private Route</h1>
+        {/* <WorkerType/> */}
+        <CreateWorkerButton />
 
-       </Sap>
+      </Sap>
     </div>
   )
 }
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       starwarsChars: []
-//     };
-//   }
-
-//   componentDidMount() {
-//     this.getCharacters("https://build-week-tess.herokuapp.com/planets/");
-//   }
-//'https://swapi.co/api/people'
-//   getCharacters = URL => {
-
-//     fetch(URL)
-//       .then(res => {
-//         return res.json();
-//       })
-//       .then(data => {
-//         this.setState({ starwarsChars: data.results });
-//       })
-//       // .catch(err => {
-//       //   throw new Error(err);
-//       // });
-//   };
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1 className="Header">React Wars</h1>
-//         <ChararcterList starwarsChars={this.state.starwarsChars} />
-        
-
-//       </div>
-//     );
-//   }
-// }
-
-
-
 
 export default CustomerPage;
